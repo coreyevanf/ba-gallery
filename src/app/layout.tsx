fa4ae6f8d25be07380f700d83a3c5ee1a06c1ba2
Cor <coreyfeduck@gmail.com>
2025-10-01 11:32:08 -0700

    added title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
-  title: "Before / After Gallery",
+  title: "Real Estate - AI Image Generator",
   description: "Before and After Image Comparison Gallery",
 };
 
